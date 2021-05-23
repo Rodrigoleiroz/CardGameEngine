e0e5998f37aca04721828c6b3d7ccfc2f16cce59
Rodrigoleiroz <76019058+Rodrigoleiroz@users.noreply.github.com>
2021-05-23 16:13:08 -0300

    torna codigo funcional

--- src/database/index.js
+++ src/database/index.js
@@ -3,18 +3,37 @@ const mongoClient = mongoDb.MongoClient;
 
 const url = 'mongodb://localhost:27017';
 
-const client = await mongoClient.connect(url)
-const db = client.db("CardGame");
-const collection = db.collection('Cards');
+const getMongoClient = async () =>{
+    const client = await mongoClient.connect(url)
+    const db = client.db("CardGame");
+    return db
+}
 
 const escreveCard = async (card) => {
+    const db = await getMongoClient()
+    const collection = db.collection('Cards');
+    // --------
     collection.insertOne(card);
 };
 
 const pegaCards = async () => {
+    const db = await getMongoClient()
+    const collection = db.collection('Cards');
+    // --------
     const cursor = collection.find({})
     const response = [];
     await cursor.forEach(doc => response.push(doc));
-    // await cursor.forEach(response.push);
+    await cursor.forEach(response.push);
     return response
 } ;
+
+let card = {
+    name: "Madara",
+    clan: "Uchiha",
+    chackra: "all"
+};
+
+const main = async () => {
+    await escreveCard(card)
+};
+main()
