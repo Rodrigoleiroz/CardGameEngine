16941d1d9f681705dc00bcb387180beddee1f2d9
Rodrigoleiroz <76019058+Rodrigoleiroz@users.noreply.github.com>
2021-05-23 16:09:04 -0300

    Usando mongoose

---
+++ src/database/mongoose.js
@@ -0,0 +1,63 @@
+const url = 'mongodb://localhost:27017';
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+});
+
+const cardSchema = new mongoose.Schema({
+    name: String,
+    clan: String,
+    chackra: String,
+  });
+const Card = mongoose.model('Card', cardSchema);
+
+// const ninja = new Card({ name: 'Naruto', clan: 'uzumaki', chackra: 'all' });
+// console.log(ninja.name);
+const main = async () => {
+    // const cardSalvo = await ninja.save();
+    const todosCards = await Card.find();
+    const todosCards2 = await Card.find({ clan:'uzumaki'});
+    const todosCards3 = await Card.deleteOne({ name: 'Naruto'});
+
+}
+
+main()
+
+// const getMongoClient = async () =>{
+//     const client = await mongoClient.connect(url)
+//     const db = client.db("CardGame");
+//     return db
+// }
+
+// const escreveCard = async (card) => {
+//     const db = await getMongoClient()
+//     const collection = db.collection('Cards');
+//     // --------
+//     collection.insertOne(card);
+// };
+
+// const pegaCards = async () => {
+//     const db = await getMongoClient()
+//     const collection = db.collection('Cards');
+//     // --------
+//     const cursor = collection.find({})
+//     const response = [];
+//     await cursor.forEach(doc => response.push(doc));
+//     await cursor.forEach(response.push);
+//     return response
+// } ;
+
+// let card = {
+//     name: "Madara",
+//     clan: "Uchiha",
+//     chackra: "all"
+// };
+
+// const main = async () => {
+//     await escreveCard(card)
+// };
+// main()
